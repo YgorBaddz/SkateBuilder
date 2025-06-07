46e23505f4b4394c2afa05b0f0c160a2d6d3628a
Ygor Badmayev <igorbadmaev2002@gmail.com>
2025-06-07 19:47:36 +0300

    feat: adding bg svgs with their effects

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -8,6 +8,8 @@ import {
 import { Bounded } from "@/components/Bounded";
 import { Heading } from "@/components/Heading";
 import { ButtonLink } from "@/components/ButtonLink";
+import { WideLogo } from "./WideLogo";
+import { TallLogo } from "./TallLogo";
 
 /**
  * Props for `Hero`.
@@ -24,6 +26,11 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       data-slice-variation={slice.variation}
       className="bg-brand-pink relative h-dvh overflow-hidden text-zinc-800 bg-texture"
     >
+      <div className="absolute inset-0 flex items-center pt-20">
+        <WideLogo className="w-full text-brand-purple hidden opacity-20 mix-blend-multiply lg:block" />
+        <TallLogo className="w-full text-brand-purple opacity-20 mix-blend-multiply lg:hidden" />
+      </div>
+
       <div className="absolute inset-0 mx-auto mt-24 grid max-w-6xl grid-rows-[1fr,auto] place-items-end px-6 py-[clamp(2.5rem,2vw+1rem,4rem)]">
         <Heading className="relative max-w-2xl place-self-start" size="lg">
           <PrismicText field={slice.primary.heading} />
